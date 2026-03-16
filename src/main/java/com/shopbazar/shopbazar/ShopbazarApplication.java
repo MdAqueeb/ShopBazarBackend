5528761d159525dfe610651ca51d453fdd970ad4
MdAqueeb <aqueebahmed505@gmail.com>
2026-03-16 21:45:21 +0530

    Project Started

---
+++ src/main/java/com/shopbazar/shopbazar/ShopbazarApplication.java
@@ -0,0 +1,13 @@
+package com.shopbazar.shopbazar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopbazarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopbazarApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/shopbazar/shopbazar/entity/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Domain models and data entities for the Shopbazar application.
+ */
+package com.shopbazar.shopbazar.entity;
